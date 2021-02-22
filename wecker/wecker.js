9d54aec4e13831db7aae49d8c0292bf838570967
Maximilian Dollinger <max.dollinger@protonmail.com>
2021-02-22 18:09:13 +0100

    alarm will not trigger fix

--- wecker/wecker.js
+++ wecker/wecker.js
@@ -1,18 +1,21 @@
 const alarmDB = require('../db/alarmModel');
 const led = require('../gpio/ledpwm');
 
-const fade = minutes => {
+const fade = fadingTime => {
      const iv = setInterval(() => {
           led.brightness++;
 
           if (led.brightness >= 100) {
                clearInterval(iv);
           }
-     }, (minutes * 600))
+     }, (fadingTime / 100))
 }
 
 const clock = setInterval(() => {
-     if (alarmDB.shouldFadingStart()) fade(alarmDB.fading);
+     if (alarmDB.shouldFadingStart()){
+          console.log(`wake started: ${alarmDB.get()}`);
+          fade(alarmDB.fading);}
+
 }, 5000);
 
 module.exports = clock;
