ddf2c942465daefa583f608dc83993c6da682e67
Maximilian Dollinger <max.dollinger@protonmail.com>
2021-03-02 10:32:01 +0100

    fixed ip filter

--- modules/setup.js
+++ modules/setup.js
@@ -1,24 +1,28 @@
 const setup = function (c) {
-        const {express, cors} = c;
+        const { express, cors } = c;
 
         const app = express();
+        app.use((req, res, next) => {
+                const ip = req.connection.remoteAddress;
+
+                if (ip.includes('192.168.178') || ip === '::1') {
+                        next();
+                } else {
+                        console.log(`blocked ip: ${ip}`);
+                        res.sendStatus(403);
+                }
+        });
         app.use(cors());
-        app.use(express.json());
+        app.use(express.json({
+                limit: '5kb',
+        }));
         app.use(express.urlencoded({ extended: true }));
         app.use(express.static('public'));
-
-        // app.use((req, res, next) => {
-        //         const ip = req.connection.remoteAddress;
-            
-        //         if(ip.includes('192.168.178')) next();
-            
-        //         res.sendStatus(403);
-        //     });
 
         const port = process.env.PORT || 8000;
         const server = () => app.listen(port, () => console.log(`${(new Date()).toLocaleString('de-DE')} => server startet on port ${port}`));
 
-        return {app, server};
+        return { app, server };
 };
 
 module.exports = setup;
