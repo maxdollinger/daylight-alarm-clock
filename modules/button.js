ddb828a799f0450e673f8c3c2facb092a2570d65
Maximilian Dollinger <max.dollinger@protonmail.com>
2021-07-15 16:39:55 +0200

    added button hold function

--- modules/button.js
+++ modules/button.js
@@ -6,14 +6,28 @@ module.exports = function ({led, pigpio, store}) {
         alert: true,
       });
 
-      let count = 0;
-
       // Level must be stable for 10 ms before an alert event is emitted.
       button.glitchFilter(10000);
-      
+
+      let count = 0;
+      let interval;
+      let direction = "up";
+
       button.on('alert', (level, tick) => {
         if (level === 0) {
-          console.log(++count);
+            interval = setInterval(() => {
+                if(direction === "up") {
+                    count += 1;
+                    if(count >= 255) direction = "down";
+                } else {
+                    count -= 1;
+                    if(count <= 1) direction = "up";
+                }
+                console.log(count)
+            }, 100)
+        }
+        if(level === 1) {
+            clearInterval(interval);
         }
       });
 
