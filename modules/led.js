0bbc834350a4bbc7c34919a34c248f1bb5ffdfb7
Maximilian Dollinger <max.dollinger@protonmail.com>
2021-07-19 11:26:28 +0200

    ...

--- modules/led.js
+++ modules/led.js
@@ -4,6 +4,8 @@ module.exports = ({ store, pigpio }) => {
     led.pwmWrite(0);
 
     store.led.subscribe((prop, value, obj) => {
+        console.log(prop, value);
+        
         if (prop === "pwm") {
             value > 255 && (value = 255);
             value < 0 && (value = 0);
@@ -31,10 +33,10 @@ module.exports = ({ store, pigpio }) => {
 
     return {
         pwm,
-        get: () => data,
+        get: () => store.led,
         post: ({ brightness }) => {
             pwm(brightness);
-            return data;
+            return store.led;
         },
         put: toggle,
     }
